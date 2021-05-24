3de3643f65e46235804d5f7bf5f495b761f42cb6
ChavdarNikolov <chavdarnikolov1065@gmail.com>
2021-05-24 10:29:02 +0300

    added String git

--- ab_SweeterIceCream/src/mainPackage/MainClass.java
+++ ab_SweeterIceCream/src/mainPackage/MainClass.java
@@ -3,5 +3,7 @@ package mainPackage;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Git commit!");
+		String git = "positionOfMarionette";
+		System.out.println(git);
 	}
 }
