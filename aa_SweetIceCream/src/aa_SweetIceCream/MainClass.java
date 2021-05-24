07c74743ca7f74f3db6caf4b9d31338d15e52288
ChavdarNikolov <chavdarnikolov1065@gmail.com>
2021-05-24 17:42:50 +0300

    Ice cream exercise

--- aa_SweetIceCream/src/aa_SweetIceCream/MainClass.java
+++ aa_SweetIceCream/src/aa_SweetIceCream/MainClass.java
@@ -1,17 +1,47 @@
 package aa_SweetIceCream;
 
 public class MainClass {
-	
 	public static void main(String[] args) {
-//		System.out.println("Hello There!");
+		int numberOfIceCreamPerDay = 10;
+		float price = 3.14f;
+		String nameOfIceCream = "EXTREME ICE CREAM";
+		String fName = "Delta";
+		boolean isLineWorking = true;
 		
+		System.out.println(numberOfIceCreamPerDay);
+		System.out.println(nameOfIceCream);
+		System.out.println(price);
+		System.out.println(isLineWorking);
 		
+		String idLetter = "A";
+		int idNumber = 12345;
 		
+		System.out.println(idLetter + idNumber);
 		
+		String[] iceCreamTaste = {"Vanilla", "Strawberry", "Banana", "Apple"};
+		arrayPrint(iceCreamTaste);
 		
-		
+		System.out.println("Hello " + fName);
 	}
 	
-	
-	
+	public static void arrayPrint(String[] collection) {
+		for(int i = 0; i < collection.length; ++i) {
+			System.out.println(collection[i]);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ab_SweeterIceCream/src/mainPackage/MainClass.java
+++ ab_SweeterIceCream/src/mainPackage/MainClass.java
@@ -2,9 +2,6 @@ package mainPackage;
 
 public class MainClass {
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		int sum = a + b;
-		System.out.println(sum);
+		
 	}
 }
