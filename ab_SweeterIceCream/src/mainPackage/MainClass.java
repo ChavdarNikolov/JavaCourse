4546e82597788ac0af151d2a8c76990e9c24f523
ChavdarNikolov <chavdarnikolov1065@gmail.com>
2021-05-24 10:44:10 +0300

    commiting changes

--- ab_SweeterIceCream/src/mainPackage/MainClass.java
+++ ab_SweeterIceCream/src/mainPackage/MainClass.java
@@ -2,8 +2,9 @@ package mainPackage;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("Git commit!");
-		String git = "positionOfMarionette";
-		System.out.println(git);
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		System.out.println(sum);
 	}
 }
